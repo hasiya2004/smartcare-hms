c7d93bfd27771692fda6eb024e798fc8c59e75c6
Hasindu Senarathna <hasindusenarathna2004@gmail.com>
2026-07-01 03:01:58 +0530

    update file structure

---
+++ src/test/java/lk/ac/sltc/smartcare/SmartcareApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sltc.smartcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
